a38112c8563d49566e35dc8e4dd709d9f8e8aed2
Git-xiaix <1326899824@qq.com>
2025-11-15 19:11:40 +0800

    position: sticky

--- Vue/miku-app-vue/src/Layouts/BasicLayouts.vue
+++ Vue/miku-app-vue/src/Layouts/BasicLayouts.vue
@@ -24,9 +24,7 @@ import GloubalFooter from '@/components/GloubalFooter.vue'
 
 <style scoped>
 #basicLayouts {
-  /* width: 1200px; */
   margin: 0 auto;
-  overflow-x: auto;
   display: flex;
   flex-direction: column;
   min-height: 100vh;

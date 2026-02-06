f7e869c8bc89f671d1c12968113d003cc409f96f
Git-xiaix <1326899824@qq.com>
2026-02-07 01:11:20 +0800

    无

--- spring/miku-spring-boot/miku-pojo/src/main/java/com/miku/dto/PageQueryDTO.java
+++ spring/miku-spring-boot/miku-pojo/src/main/java/com/miku/dto/PageQueryDTO.java
@@ -7,9 +7,9 @@ import java.io.Serializable;
 @Data
 public class PageQueryDTO implements Serializable {
 
-    //页码
+    // 页码
     private int page;
 
-    //每页显示记录数
+    // 每页显示记录数
     private int pageSize;
 }

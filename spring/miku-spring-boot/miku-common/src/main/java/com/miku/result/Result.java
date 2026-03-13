82580f58b3417f6343499b352db28735050adbef
Git-xiaix <1326899824@qq.com>
2026-03-13 13:39:50 +0800

    规范返回参数

--- spring/miku-spring-boot/miku-common/src/main/java/com/miku/result/Result.java
+++ spring/miku-spring-boot/miku-common/src/main/java/com/miku/result/Result.java
@@ -3,7 +3,7 @@ package com.miku.result;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * 后端统一返回结果
@@ -17,25 +17,25 @@ public class Result<T> implements Serializable {
     private T data; //数据
 
     public static <T> Result<T> success() {
-        Result<T> result = new Result<T>();
+        Result<T> result = new Result<>();
         result.code = 200;
-        result.data = (T) new ArrayList<>();
-        result.msg = "请求成功";
+        result.data = (T) new HashMap<String, Object>();
+        result.msg = "success";
         return result;
     }
 
-    public static <T> Result<T> success(T object) {
-        Result<T> result = new Result<T>();
+    public static <T> Result<T> success(T data) {
+        Result<T> result = new Result<>();
         result.code = 200;
-        result.data = object == null ? (T) new ArrayList<>() : object;
-        result.msg = "请求成功";
+        result.data = data;
+        result.msg = "success";
         return result;
     }
 
     public static <T> Result<T> error(String msg) {
         Result result = new Result();
         result.code = 500;
-        result.data = new ArrayList<>();
+        result.data = null;
         result.msg = msg;
         return result;
     }

2a73923a0708cfa24c2d8e88fce0145e799a3d95
Git-xiaix <1326899824@qq.com>
2026-03-14 13:37:23 +0800

    pinia全局状态初始化获取

--- Vue/miku-app-vue/src/App.vue
+++ Vue/miku-app-vue/src/App.vue
@@ -2,14 +2,27 @@
   <n-config-provider :locale="zhCN" :date-locale="dateZhCN" :theme-overrides="themeOverrides">
     <n-dialog-provider>
       <n-message-provider>
-        <BasicLayouts />
+        <!-- 初始化完成显示布局，否则显示加载中 -->
+        <template v-if="initialized">
+          <BasicLayouts />
+        </template>
+        <div v-else class="global-loading">加载中...</div>
       </n-message-provider>
     </n-dialog-provider>
   </n-config-provider>
 </template>
 <style></style>
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import { zhCN, dateZhCN } from 'naive-ui'
 import BasicLayouts from '@/Layouts/BasicLayouts.vue'
 import themeOverrides from '@/utils/theme.ts'
+import { useLoginUserStore } from '@/store/useLoginUserStore'
+
+const initialized = ref(false)
+const loginUserStore = useLoginUserStore()
+onMounted(async () => {
+  await loginUserStore.loadUserFromCache()
+  initialized.value = true
+})
 </script>

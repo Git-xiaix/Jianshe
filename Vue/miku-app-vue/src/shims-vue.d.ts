b5a7d4c97a6d3f3641d02cc00dcf8b741c108b24
Git-xiaix <1326899824@qq.com>
2026-03-23 16:22:42 +0800

    无

--- Vue/miku-app-vue/src/shims-vue.d.ts
+++ Vue/miku-app-vue/src/shims-vue.d.ts
@@ -1,26 +1,26 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent<object, object, unknown>
   export default component
 }
 
 declare module '@wangeditor/editor-for-vue' {
   import type { DefineComponent } from 'vue'
   import type { IDomEditor, IEditorConfig, IToolbarConfig } from '@wangeditor/editor'
-  
+
   interface EditorProps {
     modelValue?: string
     defaultConfig?: Partial<IEditorConfig>
     mode?: 'default' | 'simple'
     onCreated?: (editor: IDomEditor) => void
   }
-  
+
   interface ToolbarProps {
     editor?: IDomEditor
     defaultConfig?: Partial<IToolbarConfig>
     mode?: 'default' | 'simple'
   }
-  
-  export const Editor: DefineComponent<EditorProps, {}, any>
-  export const Toolbar: DefineComponent<ToolbarProps, {}, any>
+
+  export const Editor: DefineComponent<EditorProps, object, unknown>
+  export const Toolbar: DefineComponent<ToolbarProps, object, unknown>
 }
